dbe6bed3c7312072a40455b62bd2c9b33bd405bd
Krystian Kamiński <krystiankaminski07@gmail.com>
2018-11-13 20:48:39 +0100

    Some refactoring

--- script.js
+++ script.js
@@ -1,72 +1,61 @@
 class List {
     constructor(container) {
-        this.container = container || document.querySelector('body')
-        this.tasks = []
-        this.list = document.querySelector('.task-list__list')
-        this.addButton = document.querySelector('.add-task-box__button')
-        this.inputValue = document.querySelector('.add-task-box__input')
+        this.container = container || document.querySelector("body");
+        this.tasks = [];
+        this.list = document.querySelector(".task-list__list");
+        this.addButton = document.querySelector(".add-task-box__button");
+        this.inputValue = document.querySelector(".add-task-box__input");
 
-        this.init()
+        this.init();
     }
 
     init() {
-        this.render()
-        this.addButtonListener()
+        this.render();
+        this.addButtonListener();
     }
 
     addTask() {
-        this.tasks.push(new Task(this.inputValue.value, this.tasks.length))
+        this.tasks.push(new Task(this.inputValue.value, this.tasks.length));
+        console.log(this.tasks)
     }
 
     deleteTask(i) {
-        this.tasks.splice(i, 1)
+        this.tasks = this.tasks.slice(0, i).concat(this.tasks.slice(i + 1));
     }
-
 
     addButtonListener() {
-        this.addButton.addEventListener('click', () => {
-            this.addTask()
-            this.render()
-            this.inputValue.value = ''
-            console.log(this.tasks)
-            this.deleteTaskListener()
-        })
+        this.addButton.addEventListener("click", () => {
+            this.addTask();
+            this.render();
+            this.inputValue.value = "";
+        });
     }
 
-    deleteTaskListener() {
-        const deleteBtn = [...document.querySelectorAll('.task-list__item--delete')]
-        deleteBtn.forEach(btn => {
-            btn.addEventListener('click', (e) => {
-                this.list.removeChild(e.target.parentNode.parentNode)
-                console.log(this.tasks.index)
-            })
-        })
+    render() {
+        this.list.innerHTML = "";
+        this.tasks.forEach(task => {
+            task.render()
+        });
     }
+}
 
-    makeDeleteButton() {
-        return `<div class="task-list__item--delete">
+class Task {
+    constructor(text, index) {
+        this.text = text;
+        this.index = index;
+        this.parent = document.querySelector(".task-list__list")
+        this.deleteBtn = `<div class="task-list__item--delete">
         <i class="fas fa-trash-alt"></i>
     </div>`
     }
 
     render() {
-        this.list.innerHTML = ''
-        this.tasks.forEach(task => {
-            const li = document.createElement('li')
-            li.setAttribute('class', 'task-list__item')
-            li.innerHTML = task.text + this.makeDeleteButton()
-            this.list.appendChild(li)
-        })
+        const li = document.createElement("li");
+        li.setAttribute("class", "task-list__item");
+        li.innerHTML = this.text + this.deleteBtn;
+        this.parent.appendChild(li)
+        return li
     }
-
 }
 
-class Task {
-    constructor(text, index) {
-        this.text = text
-        this.index = index
-    }
-
-}
-
-const list = new List(document.querySelector('.task-list'))
+const list = new List(document.querySelector(".task-list"));
