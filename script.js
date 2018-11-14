9fc5674287b143844cd2599a83502e622b752497
Krystian Kamiński <krystiankaminski07@gmail.com>
2018-11-14 16:00:59 +0100

    Other way

--- script.js
+++ script.js
@@ -1,74 +1,38 @@
-class List {
-    constructor(container) {
-        this.container = container || document.querySelector("body");
-        this.tasks = [];
-        this.list = document.querySelector(".task-list__list");
-        this.addButton = document.querySelector(".add-task-box__button");
-        this.inputValue = document.querySelector(".add-task-box__input");
-
-        this.init();
-    }
-
-    init() {
-        this.render();
-        this.addButtonListener();
-    }
-
-    addTask() {
-        this.tasks.push(new Task(this.inputValue.value, this.tasks.length));
-    }
-
-    deleteTask(i) {
-        this.tasks = this.tasks.slice(0, i).concat(this.tasks.slice(i + 1));
-    }
-
-    addButtonListener() {
-        this.addButton.addEventListener("click", () => {
-            this.addTask();
-            this.render();
-            this.inputValue.value = "";
-            this.deleteListener()
-        });
-    }
-
-    deleteListener() {
-        const deleteBtn = [...document.querySelectorAll('.task-list__item--delete')]
-
-        deleteBtn.forEach(btn => {
-            btn.addEventListener('click', (e) => {
-                e.target.parentNode.parentNode.innerHTML = ''
-                e.preventDefault()
-                this.deleteTask(btn.id)
-                this.render()
-            })
-        })
-    }
-
-
-    render() {
-        this.list.innerHTML = "";
-        this.tasks.forEach(task => {
-            task.render()
-        });
-    }
+class App {
+  constructor(container) {
+    this.container = document.querySelector(container) || document.body;
+    this.box = new InputBox(this.container);
+    this.box.render();
+  }
 }
 
-class Task {
-    constructor(text, index) {
-        this.text = text;
-        this.index = index
-        this.parent = document.querySelector(".task-list__list")
-        this.deleteBtn = `<div id="${this.index}" class="task-list__item--delete">
-        <i class="fas fa-trash-alt"></i>
-    </div>`
-    }
+class InputBox {
+  constructor(parent) {
+    this.parent = parent;
+  }
+  render() {
+    const taskBox = document.createElement("div");
+    const title = document.createElement("h2");
+    const inputBox = document.createElement("div");
+    const input = document.createElement("input");
+    const inputBorder = document.createElement("span");
+    const button = document.createElement("button");
 
-    render() {
-        const li = document.createElement("li");
-        li.setAttribute("class", "task-list__item");
-        li.innerHTML = this.text + this.deleteBtn;
-        this.parent.appendChild(li)
-    }
+    taskBox.setAttribute("class", "add-task-box");
+    title.setAttribute("class", "add-task-box__title");
+    title.innerText = "Please add your task";
+    inputBox.setAttribute("class", "add-task-box__input-box");
+    input.setAttribute("class", "add-task-box__input");
+    input.setAttribute("placeholder", "Go to the shop...");
+    inputBorder.setAttribute("class", "add-task-box__input-border");
+    button.setAttribute("class", "add-task-box__button");
+    button.innerText = "Add task to list";
+    this.parent.appendChild(taskBox);
+    taskBox.appendChild(title);
+    taskBox.appendChild(inputBox);
+    inputBox.appendChild(input);
+    inputBox.appendChild(inputBorder);
+    taskBox.appendChild(button);
+  }
 }
-
-const list = new List(document.querySelector(".task-list"));
+const app = new App(".container");
